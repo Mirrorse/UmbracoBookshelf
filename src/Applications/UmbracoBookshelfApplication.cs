4230873c064151f8368e57e3a9e3a10c41bb0847
annahirsch <anna.hirsch@gmail.com>
2015-12-14 16:12:18 +0100

    Update UmbracoBookshelfApplication.cs
    Customized tree rendering step 4

--- src/Applications/UmbracoBookshelfApplication.cs
+++ src/Applications/UmbracoBookshelfApplication.cs
@@ -3,7 +3,7 @@ using umbraco.interfaces;
 
 namespace UmbracoBookshelf.Applications
 {
-    [Application("UmbracoBookshelf", "Umbraco Bookshelf", "icon-books", 15)]
+    [Application("UmbracoBookshelf", "Documentation", "icon-books", 15)]
     public class UmbracoBookshelfApplication : IApplication
     {
     }
