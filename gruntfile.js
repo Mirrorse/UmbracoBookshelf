c0825d129316623eeabd4775d549c0cf53f7c64a
unknown <ana@Ana-DT.mirror.public>
2016-01-27 08:52:36 +0100

    UmbracoPackage with grunt draft..

--- gruntfile.js
+++ gruntfile.js
@@ -1,204 +1,58 @@
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
-  var path = require('path')
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    pkgMeta: grunt.file.readJSON('config/meta.json'),
-    dest: grunt.option('target') || 'dist',
-    basePath: path.join('<%= dest %>', 'App_Plugins', '<%= pkgMeta.name %>'),
+    // Load the package JSON file
+    var pkg = grunt.file.readJSON('package.json');
 
-    watch: {
-      options: {
-        spawn: false,
-        atBegin: true
-      },
-	  
-	  less: {
-        files: ['src/Assets/less/*.less'],
-        tasks: ['less:dist']
-      },
+    // get the root path of the project
+    var projectRoot = 'src/';
 
-      app_plugins: {
-        files: ['src/**/**'],
-        tasks: ['copy:app_plugins', 'copy:initialContent']
-      },
+    // Load information about the assembly
+    var assembly = grunt.file.readJSON(projectRoot + 'Properties/AssemblyInfo.json');
 
-	  initialContent: {
-        files: ['src/InitialContent/*'],
-        tasks: ['copy:app_plugins', 'copy:initialContent']
-      },
+    // Get the version of the package
+    var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
 
-      dll: {
-        files: ['src/**/*.cs'],
-        tasks: ['msbuild:dist', 'copy:dll']
-      }
-    },
-	
-	less: {
-      dist: {
-        options: {
-          paths: ["src/assets/less"],
+    grunt.initConfig({
+        pkg: pkg,
+        copy: {
+            release: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: projectRoot + 'bin/Release/',
+                        src: [
+                            pkg.name + '.dll',
+                            pkg.name + '.xml'
+                        ],
+                        dest: 'files/bin/'
+                    }
+                ]
+            }
         },
-        files: {
-          '<%= basePath %>/css/application.css': 'src/assets/less/*.less',
+        umbracoPackage: {
+                src: 'files/',
+                dest: 'releases/umbraco',
+                options: {
+                    name: pkg.name,
+                    version: version,
+                    url: pkg.url,
+                    license: pkg.license.name,
+                    licenseUrl: pkg.license.url,
+                    author: pkg.author.name,
+                    authorUrl: pkg.author.url,
+                    readme: pkg.readme,
+                    manifest: 'config/package.xml',
+                    outputName: pkg.name + '.v' + version + '.zip',
+                    sourceDir: 'tmp/umbraco',
+                    outputDir: 'pkg'
+                }
         }
-      }
-    },
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-umbraco-package');
 
-    copy: {
-      app_plugins: {
-        cwd: 'src/App_Plugins/UmbracoBookshelf',
-        src: ['**', '!css', '!css/*'],
-        dest: '<%= basePath %>',
-        expand: true
-      },
-      dll: {
-        cwd: 'src/bin/',
-        src: 'UmbracoBookshelf.dll',
-        dest: '<%= dest %>/bin/',
-        expand: true
-      },
-	  actionsDll: {
-        cwd: 'lib/',
-        src: 'PackageActionsContrib.dll',
-        dest: '<%= dest %>/bin/',
-        expand: true
-      },
-	  initialContent: {
-        cwd: 'src/InitialContent/UmbracoBookshelf/',
-        src: ['**'],
-        dest: '<%= dest %>/UmbracoBookshelf/',
-        expand: true
-      },
-      nuget: {
-        files: [
-          {
-            cwd: '<%= dest %>/App_Plugins',
-            src: ['**/*', '!bin', '!bin/*'],
-            dest: 'tmp/nuget/content/App_Plugins',
-            expand: true
-          },
-		  {
-            cwd: '<%= dest %>/UmbracoBookshelf/',
-            src: ['**/*'],
-            dest: 'tmp/nuget/content/UmbracoBookshelf',
-            expand: true
-          },
-          {
-            cwd: '<%= dest %>/bin',
-            src: ['*.dll'],
-            dest: 'tmp/nuget/lib/net40',
-            expand: true
-          }
-        ]
-      },
-      umbraco: {
-        cwd: '<%= dest %>',
-        src: '**/*',
-        dest: 'tmp/umbraco',
-        expand: true
-      }
-    },
+    grunt.registerTask('dev', ['copy', 'umbracoPackage']);
+    grunt.registerTask('default', ['dev']);
 
-    nugetpack: {
-    	dist: {
-    		src: 'tmp/nuget/package.nuspec',
-    		dest: 'pkg'
-    	}
-    },
-
-    template: {
-    	'nuspec': {
-			'options': {
-    			'data': { 
-					name: '<%= pkgMeta.name %>',
-					version: '<%= pkgMeta.version %>',
-					url: '<%= pkgMeta.url %>',
-					license: '<%= pkgMeta.license %>',
-					licenseUrl: '<%= pkgMeta.licenseUrl %>',
-					author: '<%= pkgMeta.author %>',
-					authorUrl: '<%= pkgMeta.authorUrl %>',
-					files: [{ path: 'tmp/nuget/content/App_Plugins', target: 'content/App_Plugins'}]
-	    		}
-    		},
-    		'files': { 
-    			'tmp/nuget/package.nuspec': ['config/package.nuspec']
-    		}
-    	}
-    },
-
-    umbracoPackage: {
-      options: {
-        name: "<%= pkgMeta.name %>",
-        version: '<%= pkgMeta.version %>',
-        url: '<%= pkgMeta.url %>',
-        license: '<%= pkgMeta.license %>',
-        licenseUrl: '<%= pkgMeta.licenseUrl %>',
-        author: '<%= pkgMeta.author %>',
-        authorUrl: '<%= pkgMeta.authorUrl %>',
-        manifest: 'config/package.xml',
-        readme: 'config/readme.txt',
-        sourceDir: 'tmp/umbraco',
-        outputDir: 'pkg',
-      }
-    },
-
-    clean: {
-      build: '<%= grunt.config("basePath").substring(0, 4) == "dist" ? "dist/**/*" : "null" %>',
-      tmp: ['tmp']
-    },
-
-    assemblyinfo: {
-      options: {
-        files: ['src/UmbracoBookshelf.csproj'],
-        filename: 'AssemblyInfo.cs',
-        info: {
-          version: '<%= (pkgMeta.version.indexOf("-") ? pkgMeta.version.substring(0, pkgMeta.version.indexOf("-")) : pkgMeta.version) %>', 
-          fileVersion: '<%= pkgMeta.version %>'
-        }
-      }
-    },
-
-    touch: {
-      webconfig: {
-        src: ['<%= grunt.option("target") %>\\Web.config']
-      }
-    },
-
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      src: {
-        src: ['app/**/*.js', 'lib/**/*.js']
-      }
-    },
-
-    msbuild: {
-      options: {
-        stdout: true,
-        verbosity: 'quiet',
-        maxCpuCount: 4,
-		version: 4.0,
-        buildParameters: {
-          WarningLevel: 2,
-          NoWarn: 1607
-        }
-      },
-      dist: {
-        src: ['src/UmbracoBookshelf.csproj'],
-        options: {
-          projectConfiguration: 'Debug',
-          targets: ['Clean', 'Rebuild'],
-        }
-      }
-    }
-
-  });
-
-  grunt.registerTask('default', ['clean', 'less', 'assemblyinfo', 'msbuild:dist', 'copy:initialContent', 'copy:dll', 'copy:actionsDll', 'copy:app_plugins']);
-  grunt.registerTask('nuget',   ['clean:tmp', 'default', 'copy:nuget', 'template:nuspec', 'nugetpack']);
-  grunt.registerTask('umbraco', ['clean:tmp', 'default', 'copy:umbraco', 'umbracoPackage']);
-  grunt.registerTask('package', ['clean:tmp', 'default', 'copy:nuget', 'template:nuspec', 'nugetpack', 'copy:umbraco', 'umbracoPackage', 'clean:tmp']);
 };
